15ff8d32df4ace7e2ed60cf0984938942d94c98a
Wellington Fernandes <wellsfernandes1991@gmail.com>
2023-06-28 13:11:05 -0300

    Tema claro
    Ajuste na cor do tema claro

--- app/themeMode.js
+++ app/themeMode.js
@@ -2,6 +2,9 @@ function setDarkTheme() {
     const root = document.documentElement;
     const currentTime = new Date().getHours();
     const isDarkModeEnabled = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    imagemLinkedin = document.getElementById('imagem-linkedin');
+    imagemGithub = document.getElementById('imagem-github');
+    imagemEmail = document.getElementById('imagem-email');
   
     if (isDarkModeEnabled || (currentTime >= 18 || currentTime < 6)) {
       root.style.setProperty('--primary-color', '#e0e0e0');
@@ -13,16 +16,22 @@ function setDarkTheme() {
       root.style.setProperty('--background-secondary-color', '#272727');
       root.style.setProperty('--background-tertiary-color', '#494949');
       root.style.setProperty('--background-quaternary-color', '#727272');
+      imagemLinkedin.src = "assets/linkedin.png";
+      imagemGithub.src = "assets/github.png";
+      imagemEmail.src = "assets/email.png";
     } else {
       root.style.setProperty('--primary-color', '#292929');
       root.style.setProperty('--secondary-color', '#007bff');
       root.style.setProperty('--tertiary-color', '#555555');
       root.style.setProperty('--quaternary-color', '#f5f5f5');
       root.style.setProperty('--background-color', '#c9c9c9');
-      root.style.setProperty('--background-primary-color', '#ababab');
-      root.style.setProperty('--background-secondary-color', '#c1c1c1');
-      root.style.setProperty('--background-tertiary-color', '#dddddd');
-      root.style.setProperty('--background-quaternary-color', '#e9e9e9');
+      root.style.setProperty('--background-primary-color', '#F5F5F5');
+      root.style.setProperty('--background-secondary-color', '#E8E8E8');
+      root.style.setProperty('--background-tertiary-color', '#DADADA');
+      root.style.setProperty('--background-quaternary-color', '#FFFFFF');
+      imagemLinkedin.src = "assets/linkedin-black.png";
+      imagemGithub.src = "assets/github-black.png";
+      imagemEmail.src = "assets/email-black.png";
     }
   }
   
