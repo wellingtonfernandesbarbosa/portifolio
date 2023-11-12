83858864b00504d86c7b23bf073160c5ed3a9c70
Wellington Fernandes <wellingtonfernandes@protonmail.com>
2023-11-11 21:44:58 -0300

    Implementation of Image Alt Text Translation and Tooltip Titles in Skills Section
    In this commit, I implemented the translation of alternative text for images based on language selection. Additionally, I added titles to the technology icons in the skills section, allowing users to view tooltips when hovering over them. This enhances accessibility and provides additional information for a better user experience.

--- app/languageSet.js
+++ app/languageSet.js
@@ -4,8 +4,15 @@ document.addEventListener("DOMContentLoaded", function () {
   if (userLang === "en-US" || userLang === "en") {
     let elementsToTranslate = document.querySelectorAll("[data-translate-en]");
 
-    elementsToTranslate.forEach(function (element) {
+    elementsToTranslate.forEach((element) => {
       element.innerHTML = element.getAttribute("data-translate-en");
+    });
+
+    let imagesToTranslate = document.querySelectorAll("img[data-translate-en-alt]");
+
+    imagesToTranslate.forEach((image) => {
+      let altText = image.getAttribute("data-translate-en-alt");
+      image.alt = altText;
     });
   }
 });
